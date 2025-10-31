ae19e54d1d288153c995bccc53ddd0981163d670
raito <2434.raito@gmail.com>
2025-10-31 10:34:31 +0900

    テスト項目追加

--- test.bash
+++ test.bash
@@ -1,15 +1,20 @@
 #!/bin/bash
 # SPDX-FileCopyrightText: 2025 raito kaneko 2434.raito@gmial.com
 # SPDX-License-Identifier: BSD-3-Clause
-ng(){
-	echo ${1}行目が違うよ
-	res=1
-}
+### NORMAL INPUT ###
+out=$(seq 5 | ./plus)
+[ "${out}" = 15.0 ] || ng "$LINENO"
+
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]    || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]	|| ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
 res=0
 
-out=$(seq 5 | ./plus_stdin)
-[ "${out}" = 15 ] || ng "$LINENO"
-
-[ "${res}" = 0 ] && echo OK
+[ "$res" = 0 ] && echo OK
 exit $res
