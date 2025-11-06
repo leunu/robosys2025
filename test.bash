e9b0b8a170e2fde2bc18bc439aa0acec06ebf8de
raito <2434.raito@gmail.com>
2025-11-06 12:06:06 +0900

    test.bash及びplusのエラー修正

--- test.bash
+++ test.bash
@@ -1,20 +1,26 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2025 raito kaneko 2434.raito@gmial.com
 # SPDX-License-Identifier: BSD-3-Clause
-### NORMAL INPUT ###
-out=$(seq 5 | ./plus)
-[ "${out}" = 15.0 ] || ng "$LINENO"
 
-### STRANGE INPUT ###
-out=$(echo あ | ./plus)
-[ "$?" = 1 ]    || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
-
-out=$(echo | ./plus)
-[ "$?" = 1 ]	|| ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
+ng (){
+        echo ${1}行目がちがう
+        res= 1
+}
 
 res=0
+###NORMAL INPUT###
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng "$LINENO"
 
-[ "$res" = 0 ] && echo OK
+###STRANGE INPUT###
+out=$(echo あ | ./plus)
+["$?" = 1]      || ng "$LINENO"
+["${out}" = "" ] || ng "$LINENO"
+
+out=$(echo | ./plus)
+["$?" = 1]      || ng "$LINENO"
+["${out}" = "" ] || ng "$LINENO"
+
+[ "${res}" = 0 ] && echo OK
+
 exit $res
